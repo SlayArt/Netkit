7328abfd031bd67cc56ebb046f2d4f15e5ac019c
slayart <slayartftn@gmail.com>
2025-10-11 14:46:10 +0200

    src/main.c modified : add argument's limit for Netkti | src/hexdump.c modified : final version hexdump, hexdump usable

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,8 +1,56 @@
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 #include "hexdump.h"
+
+#define LINE_BYTES 16
 
 int nk_hexdump(const char *filename) {
     printf("opening file %s \n", filename);
+
+    FILE *inputFile = fopen(filename, "rb");
+    if (!inputFile) {
+        perror("Error opening file ");
+        return 1;
+    }
+
+
+    unsigned char buffer[LINE_BYTES];
+    size_t offset = 0;
+    size_t n;
+
+    while ((n = fread(buffer, 1, LINE_BYTES, inputFile)) > 0) {
+        
+        printf("%08zx\t", offset);
+
+        for (size_t i = 0; i < n; ++i) {
+            printf("%02x ", buffer[i]);
+            if (i == 7) {printf(" ");}
+        } printf("\t");
+        
+        if (n < LINE_BYTES) {
+            for (size_t i = n; i < LINE_BYTES; i++) {
+                printf("    ");
+            }
+        }
+
+        for (size_t i = 0; i < n; i++) {
+            unsigned char ch = buffer[i];
+            if (isprint(ch)) {
+                putchar(ch);
+            } else {printf(".");}
+        }
+
+        printf("\n");
+        offset += n;
+    }
+    
+    printf("\n%zu bytes read \n", offset);
+    printf("Hexdump used with succes on %s\n", filename);
+
+    fclose(inputFile);
 
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -4,14 +4,21 @@
 #include "hexdump.h"
 
 int main(int argc, char *argv[]) {
-	printf("NetKit ready to use !!! \n\n");
     
-    if (argc > 2 && strcmp(argv[1], "hexdump") == 0) {
+    if (argc > 3) {
+        printf("too many argument for NetKit...\n");
+        return 1;
+    } 
+
+	printf("NetKit ready to use... \n\n");
+    printf("----- \n\n");
+    
+    if (argc == 3 && strcmp(argv[1], "hexdump") == 0) {
         printf("hexdump in use... \n");
         int result = nk_hexdump(argv[2]);
         
         if (result != 0) {
-            printf("[ERROR] can't use hexdump with %c", *argv[2]);
+            printf("[ERROR] can't use hexdump with %s \n", argv[2]);
         }
     }
    
